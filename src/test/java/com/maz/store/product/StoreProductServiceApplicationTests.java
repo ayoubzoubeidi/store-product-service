0bfac1b7e768459f02523fde8fd4302389eecd82
ayoub <ayoubzoubeidi@gmail.com>
2021-08-12 11:56:33 +0100

    Initial project commit. closes #1

---
+++ src/test/java/com/maz/store/product/StoreProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maz.store.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
