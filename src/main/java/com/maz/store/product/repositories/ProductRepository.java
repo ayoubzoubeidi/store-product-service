05113d534d05251f9e85fc5e1e62203b6da651af
ayoub <ayoubzoubeidi@gmail.com>
2021-08-23 14:56:50 +0100

    Added validation listener business logic. TODO add test

--- src/main/java/com/maz/store/product/repositories/ProductRepository.java
+++ src/main/java/com/maz/store/product/repositories/ProductRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends ReactiveMongoRepository<Product, UUID> {
+
+    Boolean existsAllByUpc(List<String> upcs);
+
 }
--- src/main/java/com/maz/store/product/services/ValidationListener.java
+++ src/main/java/com/maz/store/product/services/ValidationListener.java
@@ -1,22 +1,37 @@
 package com.maz.store.product.services;
 
+import com.maz.store.model.order.OrderLineDto;
 import com.maz.store.model.product.ValidationRequest;
+import com.maz.store.model.product.ValidationResponse;
 import com.maz.store.product.config.JmsConfig;
 import com.maz.store.product.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ValidationListener {
 
+    private final JmsTemplate jmsTemplate;
     private final ProductRepository productRepository;
 
     @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
     public void validationListener(ValidationRequest request) {
+
+        Boolean isValid = productRepository.existsAllByUpc(request.getOrder().getOrderLines()
+                .stream()
+                .map(OrderLineDto::getUpc)
+                .collect(Collectors.toList()));
+
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATION_RESPONSE_QUEUE,
+                ValidationResponse.builder().orderId(request.getOrder().getId()).isValid(isValid).build());
+
     }
 
 }
